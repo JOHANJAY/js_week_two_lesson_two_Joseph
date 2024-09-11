b861bf95c61829dc46bf977385032e090f718ce5
John Joseph <joseph.john33@yahoo.com>
2024-09-11 12:55:50 +0100

    submmiting lesson two

---
+++ index.js
@@ -0,0 +1,96 @@
+//Task One
+/* let person = {}
+
+person.name = "Emmanuel"
+person.age = 27
+person.job = "Front-end development"
+person.location = "Abuja"
+
+console.log(person);
+ */
+
+//Task Two
+/* let person = {
+  name: "Emmanuel",
+  age: 27,
+  job: "Front-end development",
+  location: "Abuja",
+};
+
+person.location = "kubwa"
+
+console.log(person);
+ */
+
+//Task Three
+/* let person = {
+  name: "Emmanuel",
+  age: 27,
+  job: "Front-end development",
+  location: "Abuja",
+};
+
+for (const key in person) {
+    console.log(`${key}: ${person[key]}`);
+} */
+
+//Task Four
+/* let person = {
+  name: "Emmanuel",
+  age: 27,
+  job: "Front-end development",
+  location: "Abuja",
+};
+
+delete person.job
+console.log(person);
+ */
+
+//Task Five
+/* let person = {
+  name: "Emmanuel",
+  age: 27,
+  job: "Front-end development",
+  location: "Abuja",
+};
+
+let student = {
+    name: "Jay",
+    job: "Full-stack developer",
+}
+
+const people = Object.assign(person, student)
+
+console.log(people);
+ */
+
+//Task Six
+//i
+/* if (x == 5) {
+  console.log("x is equal to 5");
+}
+
+//ii
+const message = "Hello, world";
+
+console.log(message); */
+
+//iii
+/* function sayHello() {
+  console.log("Hello!");
+
+  console.log("Goodbye");
+} */
+
+//iv
+/* const numbers = [1, 2, 3, 4, 5];
+console.log(numbers[2]);
+ */
+
+//v
+/* const addNumbers = function (x, y) {
+    return x + y
+}
+
+console.log(addNumbers(5,10));
+ */
